7039568a9d593ec9eb869818cf7b4ae1cea36e6e
hypolas <nicolas.hypolite@gmail.com>
2023-03-04 14:23:54 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module hypolaslogger
+
+go 1.20
---
+++ log_test.go
@@ -0,0 +1,38 @@
+package hypolaslogger
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+)
+
+const (
+	testfile = "test/test.txt"
+)
+
+// Test if log is wheel write.
+func TestLogs(t *testing.T) {
+	// Remove existing test file
+	os.Remove(testfile)
+
+	log := NewLogger(testfile)
+	log.Info.Println("Info")
+	log.Warn.Println("Warn")
+	log.Err.Println("Err")
+
+	readFile, err := os.Open(testfile)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	readFile.Close()
+}
