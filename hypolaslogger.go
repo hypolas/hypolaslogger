75604447432b23a188352482b68ebdd471e8a749
hypolas <nicolas.hypolite@gmail.com>
2023-03-04 14:32:29 +0100

    Change module path

---
+++ hypolaslogger.go
@@ -0,0 +1,29 @@
+package hypolaslogger
+
+import (
+	"log"
+	"os"
+)
+
+type HypolasLogger struct {
+	Info    *log.Logger
+	Warn    *log.Logger
+	Err     *log.Logger
+	LogFile *os.File
+}
+
+// Create logs. PATH is the file path where logs will be store
+func NewLogger(path string) HypolasLogger {
+	var err error
+	l := HypolasLogger{}
+	l.LogFile, err = os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l.Info = log.New(l.LogFile, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	l.Warn = log.New(l.LogFile, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	l.Err = log.New(l.LogFile, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return l
+}
